9db3359528931ddbb255eba4bfb9ef97662d2c64
Jarod Peachey <jwpeachey107@aol.com>
2019-04-02 15:16:09 -0400

    Update style loaders

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import '../style.css'
 
 class App extends Component {
    render() {
       return (
          <div>
-            <h1>{new Date().getTime()}</h1>
+            <div id="content">
+               <h1>React Starter Template</h1>
+               <h3>Congrats!  You're on React!  Get started by changing the app.js file to display whatever you want on this page.</h3>
+            </div>
          </div>
       )
    }

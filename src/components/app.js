2d739979760718b2052635727ded52d000d7ce94
Jarod Peachey <jwpeachey107@aol.com>
2019-04-01 21:24:24 -0400

    First commit - Everything finally works!

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+   render() {
+      return (
+         <div>
+            <h1>This is my first react app with an edited file.</h1>
+         </div>
+      )
+   }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+   entry: path.join(__dirname, "src", "index.js"),
+   output: {
+      path: path.join(__dirname, "build"),
+      filename: "bundle.js"
+   },
+   module: {
+      rules: [
+         {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,               loader: "babel-loader",
+            options: {
+               presets: ["@babel/preset-env", "@babel/preset-react"]
+            }
+         }
+      ]
+   },
+   plugins: [
+     new HtmlWebpackPlugin({
+       filename: "index.html",
+       template: path.join(__dirname, "src", "index.html")
+     })
+   ]
+}

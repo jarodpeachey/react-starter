d835cb74ac6f9e93a273c714577779db64b0f94f
Jarod Peachey <jwpeachey107@aol.com>
2019-04-05 14:24:54 -0400

    Update dependencies and everything

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,33 @@
+/* eslint-disable linebreak-style */
 const path = require('path');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-   entry: path.join(__dirname, "src", "index.js"),
-   output: {
-      path: path.join(__dirname, "build"),
-      filename: "bundle.js"
-   },
-   module: {
-      rules: [
-         {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,               loader: "babel-loader",
-            options: {
-               presets: ["@babel/preset-env", "@babel/preset-react"]
-            }
-         },
-         {
-            test: /\.css/,
-            use: [
-               { loader: 'style-loader' },
-               { loader: 'css-loader' }
-            ]
-         }
-      ]
-   },
-   plugins: [
-     new HtmlWebpackPlugin({
-       filename: "index.html",
-       template: path.join(__dirname, "src", "index.html")
-     })
-   ]
-}
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        // options: {
+        //   presets: ['@babel/preset-env', '@babel/preset-react'],
+        // },
+      },
+      {
+        test: /\.css/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};

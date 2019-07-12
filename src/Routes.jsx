7d4142253ac8a58ea9e49e632ff6f5c2edd3e917
Jarod Peachey <jwpeachey107@aol.com>
2019-07-12 15:45:16 -0400

    Revert "Update to use router and other dependencies"
    This reverts commit f2ffccd79ed486652f592181b9d3b1c64ac40ca0.

--- src/Routes.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Route } from 'react-router';
-import Application from './Application';
-
-const routes = () => (
-  <Route path="/" component={Application} />
-);
-
-export default routes;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,5 @@
-/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import { render } from 'react-dom';
-import {
-  browserHistory, Router,
-} from 'react-router';
-// import { useScroll } from 'react-router-scroll';
-import { MuiThemeProvider } from '@material-ui/core/styles';
-import { ThemeProvider } from 'styled-components';
-import muiTheme from './mui-theme';
-import styledTheme from './styled-theme';
-import routes from './Routes';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-function startApp () {
-  render(
-    <MuiThemeProvider theme={muiTheme}>
-      <ThemeProvider theme={styledTheme}>
-        <Router
-          history={browserHistory}
-        >
-          {routes()}
-        </Router>
-      </ThemeProvider>
-    </MuiThemeProvider>, document.getElementById('app'),
-  );
-}
-
-startApp();
+ReactDOM.render(<App />, document.getElementById('app'));
